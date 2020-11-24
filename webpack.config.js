5854bfead5267c2cc97f30c14ebe99d817f7b671
serg8master8 <apple500538@gmail.com>
2020-11-24 14:52:41 +0300

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -21,17 +21,40 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
+    new HTMLWebpackPlugin({
       template: 'index.html'
     }),
-    new CopyPlugin([
+    new CopyPlugin({
+      patterns: [
       {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
+      from: path.resolve(__dirname, 'src/favicon.ico'),
+      to: path.resolve(__dirname, 'dist'),
+      },
+      ],
+      }),
     new MiniCssExtractPlugin({
       filename: 'bundle.[hash].css'
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
